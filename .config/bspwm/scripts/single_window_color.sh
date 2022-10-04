8b7599c2ff3d5712bb56898a05a2712c2611d508
Eric Sandoval <esandov8@ucsc.edu>
2022-10-04 02:44:58 -0700

    Improve solution to hide borders when only 1 window

--- .config/bspwm/scripts/single_window_color.sh
+++ .config/bspwm/scripts/single_window_color.sh
@@ -4,8 +4,8 @@ bspc subscribe desktop_layout desktop_focus node_add node_remove node_stack node
   count=$(bspc query -N -d focused -n .window.\!hidden | wc -l)
   echo $count
   if [ $count == 1 ]; then
-    bspc config border_width 0
+    bspc config focused_border_color "#282c34"
   else
-    bspc config border_width 2
+    bspc config focused_border_color "#61afef"
   fi
 done
