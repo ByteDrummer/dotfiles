e4d14532cba8e57b63675821467636eda6a7f9f2
Eric Sandoval <esandov8@ucsc.edu>
2022-10-06 03:45:45 -0700

    Fix screenshot naming

--- .config/bspwm/scripts/screenshot.sh
+++ .config/bspwm/scripts/screenshot.sh
@@ -11,10 +11,10 @@ options="$option0\n$option1\n$option2"
 selected="$(echo -e "$options" | rofi -lines 3 -dmenu -p "scrot" -theme-str 'window {width: 10%;}')"
 case $selected in
     $option0)
-        cd $(xdg-user-dir PICTURES)/ && sleep 1 && scrot;;
+        cd $(xdg-user-dir PICTURES)/ && sleep 1 && scrot ~/Pictures/Screenshots/Screenshot-$(date +%F_%T).png;;
     $option1)
-        cd $(xdg-user-dir PICTURES)/ && scrot --select --line mode=edge;;
+        cd $(xdg-user-dir PICTURES)/ && scrot --select --line mode=edge ~/Pictures/Screenshots/Screenshot-$(date +%F_%T).png;;
     $option2)
-        cd $(xdg-user-dir PICTURES)/ && sleep 1 && scrot -u;;
+        cd $(xdg-user-dir PICTURES)/ && sleep 1 && scrot -u ~/Pictures/Screenshots/Screenshot-$(date +%F_%T).png;;
 esac
 
