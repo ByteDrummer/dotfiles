c446ce95a23d923e45063b76493dc2594df9e15e
Eric Sandoval <esandov8@ucsc.edu>
2022-10-11 23:26:13 -0700

    Adjust notification sound rules

--- .config/dunst/play_notification.sh
+++ .config/dunst/play_notification.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+
+# The script will be called as follows:
+#   script appname summary body icon urgency
+
 if [[ "$1" != "Spotify" &&
       "$1" != "discord" &&
-      "$1" != "Google Chrome" &&
+      "$3" != *"calendar.google.com"* &&
       "$1" != "Thunderbird" ]]; then
   paplay ~/.config/dunst/notification.ogg
 fi
