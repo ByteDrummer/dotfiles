59668fe253773f914f8a86b287bcbeacebcc2d47
Eric Sandoval <esandov8@ucsc.edu>
2022-10-11 18:40:03 -0700

    Update notification rules

--- .config/dunst/play_notification.sh
+++ .config/dunst/play_notification.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 if [[ "$1" != "Spotify" &&
       "$1" != "discord" &&
+      "$1" != "Google Chrome" &&
       "$1" != "Thunderbird" ]]; then
   paplay ~/.config/dunst/notification.ogg
 fi
