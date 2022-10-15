acfdba8626249f5bc847629adac77c6f9c93ec50
Eric Sandoval <esandov8@ucsc.edu>
2022-10-14 20:37:49 -0700

    Remove xfce power manager in favor of manual DPMS config and replace xssproxy with caffeine-ng

--- setup.sh
+++ setup.sh
@@ -49,7 +49,7 @@ sudo pacman -S tmux
 sudo pacman -S vivid
 
 # Install additional desktop software
-yay -S blueman gthumb xfce4-power-manager arandr playerctl xss-lock betterlockscreen xssproxy
+yay -S blueman gthumb arandr playerctl xss-lock betterlockscreen caffeine-ng
 
 # Disable the display manager
 sudo rm /etc/systemd/system/display-manager.service
