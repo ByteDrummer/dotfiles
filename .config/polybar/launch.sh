152691c9170560680df4005460d0bdc42f1b79d1
Eric Sandoval <esandov8@ucsc.edu>
2022-12-04 22:43:08 -0800

    Make icon tray attach to primary monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,10 +7,20 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar
-#polybar top -c ~/.config/polybar/config.ini &
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+  monitors="$(xrandr --query | grep " connected")"
+
+  # Reorder list so icon tray attaches to the bar for the primary monitor
+  if echo "$monitors" | grep "primary"; then
+    temp="$monitors"
+    monitors="$(echo "$temp" | grep "primary")"
+    monitors+=$'\n'
+    monitors+="$(echo "$temp" | grep -v "primary")"
+  fi
+
+  for m in $(echo "$monitors" | cut -d" " -f1); do
     MONITOR=$m polybar --reload top &
+    sleep 0.1 # Allow time for tray to attach to first bar
   done
 else
   polybar --reload top &
