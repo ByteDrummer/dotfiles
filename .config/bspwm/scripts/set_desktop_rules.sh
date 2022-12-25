d9aa6c8c8dcbeac83c2a46695b69fefbd845368c
Eric Sandoval <esandov8@ucsc.edu>
2022-12-25 00:35:49 -0800

    Revert to original solution for managing nodes

--- .config/bspwm/scripts/set_desktop_rules.sh
+++ .config/bspwm/scripts/set_desktop_rules.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-monitor=""
+primary_monitor=$(xrandr | grep primary | sed 's/ .*//')
 
-if [ "$1" = "duplicate" ]; then
-  monitor="$(bspc query -M --names)"
-else
-  monitor="$(xrandr | grep primary | sed 's/ .*//')"
-fi
-
-bspc rule -a thunderbird desktop="$monitor:^1"
-bspc rule -a "*:crx_kjbdgfilnfhdoflbpgamdcdgpehopbep" desktop="$monitor:^1"
-bspc rule -a "*:crx_mgamiaabcidhdjkgbmmalofnegcnbbpi" desktop="$monitor:^2"
-bspc rule -a discord desktop="$monitor:^2"
-bspc rule -a Lutris desktop="$monitor:^5"
-bspc rule -a Steam desktop="$monitor:^5"
+bspc rule -a thunderbird desktop="$primary_monitor:^1"
+bspc rule -a "*:crx_kjbdgfilnfhdoflbpgamdcdgpehopbep" desktop="$primary_monitor:^1"
+bspc rule -a "*:crx_mgamiaabcidhdjkgbmmalofnegcnbbpi" desktop="$primary_monitor:^2"
+bspc rule -a discord desktop="$primary_monitor:^2"
+bspc rule -a Lutris desktop="$primary_monitor:^5"
+bspc rule -a Steam desktop="$primary_monitor:^5"
