64ad5fa52c9302bd2fbe293d62c79a4dee7144b7
Eric Sandoval <esandovalruezga@gmail.com>
2023-01-29 17:01:54 -0800

    Set qt theme using kvantum

---
+++ .profile
@@ -0,0 +1,2 @@
+# set QT theme to be kvantum's
+export QT_STYLE_OVERRIDE=kvantum
--- setup.sh
+++ setup.sh
@@ -45,7 +45,7 @@ sudo pacman -S tmux
 sudo pacman -S vivid
 
 # Install additional desktop software
-yay -S blueman gthumb arandr playerctl betterlockscreen caffeine-ng xidlehook light autorandr
+yay -S blueman gthumb arandr playerctl betterlockscreen caffeine-ng xidlehook light autorandr kvantum
 
 # Needed for light
 sudo usermod -aG video $USER
