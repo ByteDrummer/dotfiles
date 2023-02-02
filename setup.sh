a8ac84a4c8b15716b7796b175a35703a287986c2
Eric Sandoval <esandovalruezga@gmail.com>
2023-02-01 17:23:56 -0800

    Update qt theme

--- setup.sh
+++ setup.sh
@@ -45,7 +45,7 @@ sudo pacman -S tmux
 sudo pacman -S vivid
 
 # Install additional desktop software
-yay -S blueman gthumb arandr playerctl betterlockscreen caffeine-ng xidlehook light autorandr kvantum
+yay -S blueman gthumb arandr playerctl betterlockscreen caffeine-ng xidlehook light autorandr kvantum qt5ct qt6ct kvantum-theme-libadwaita-git
 
 # Needed for light
 sudo usermod -aG video $USER
