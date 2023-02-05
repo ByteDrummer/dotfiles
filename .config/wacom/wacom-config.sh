69fac81f0123e98911e784f28bd91f9784dbc7d3
Eric Sandoval <esandov8@ucsc.edu>
2023-02-04 21:29:31 -0800

    Account for all variations in tablet width

--- .config/wacom/wacom-config.sh
+++ .config/wacom/wacom-config.sh
@@ -11,10 +11,19 @@ primary_monitor=$(xrandr | grep primary | sed 's/ .*//')
 list=$(xsetwacom list devices)
 stylus=$(echo "${list}" | grep stylus | sed 's/\s*id.*//')
 tablet_width=$(xsetwacom get "$stylus" Area | cut -d " " -f 3)
+tablet_height=$(xsetwacom get "$stylus" Area | cut -d " " -f 4)
+tablet_ratio=$(perl -e "print $tablet_width / $tablet_height")
 screen_dim=$(xrandr | grep "connected primary" | cut -d " " -f 4)
 screen_width=$(echo "$screen_dim" | sed 's/x.*//')
 screen_height=$(echo "$screen_dim" | sed 's/.*x\(.*\)+.*+.*/\1/')
-tablet_height=$(perl -e "print $tablet_width * $screen_height / $screen_width")
+screen_ratio=$(perl -e "print $screen_width / $screen_height")
 
 xsetwacom set "$stylus" MapToOutput "$primary_monitor"
-xsetwacom set "$stylus" Area 0 0 $tablet_width $tablet_height
+
+if [[ $tablet_ratio < $screen_ratio ]]; then # if tablet is thinner
+  area_height=$(perl -e "print $tablet_width / $screen_ratio")
+  xsetwacom set "$stylus" Area 0 0 $tablet_width $area_height
+else # if tablet is wider
+  area_width=$(perl -e "print $tablet_height * $screen_ratio")
+  xsetwacom set "$stylus" Area 0 0 $area_width $tablet_height
+fi
