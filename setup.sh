6b9a4efdad0f70c1f19e6450770f6f6c7def897e
Eric Sandoval <esandovalruezga@gmail.com>
2023-05-08 12:48:38 -0700

    Simplify gtk theme setup

--- setup.sh
+++ setup.sh
@@ -73,13 +73,6 @@ yay -S kvantum qt5ct qt6ct
 
 # Setup gtk theme
 yay -S adw-gtk3
-wget https://github.com/lonr/adwaita-one-dark/releases/download/v0.44.0/Adwaita-One-Dark.tar.xz -P ~/Downloads/
-mkdir ~/.local/share/themes/Adwaita-One-Dark
-tar -xf ~/Downloads/Adwaita-One-Dark.tar.xz -C ~/.local/share/themes/Adwaita-One-Dark/
-ln -s ~/.local/share/themes/Adwaita-One-Dark/colors/gtk-dark.css ~/.config/gtk-4.0/gtk.css
-ln -s ~/.local/share/themes/Adwaita-One-Dark/colors/gtk-dark.css ~/.config/gtk-3.0/gtk.css
-mkdir -p ~/.themes/adw-gtk3-dark
-ln -s ~/.local/share/themes/Adwaita-One-Dark/gtk-2.0 ~/.themes/adw-gtk3-dark/gtk-2.0
 sudo ln -s ~/.config/gtk-3.0 /root/.config/gtk-3.0
 
 
