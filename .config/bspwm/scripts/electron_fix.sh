8a177592e282987b8f645bd88a1da0f51b5f7f74
Eric Sandoval <esandovalruezga@gmail.com>
2023-05-12 20:56:13 -0700

    Improve electron_fix.sh algorithm

--- .config/bspwm/scripts/electron_fix.sh
+++ .config/bspwm/scripts/electron_fix.sh
@@ -1,48 +1,36 @@
 #!/usr/bin/env bash
 
-# Workaround for electron and chrome window focusing bug
+# Workaround for electron and chromium window focusing bug
 # See link for more info: https://github.com/baskerville/bspwm/issues/811
-# Make sure to turn off hardware acceleration in chrome for better refocusing
 
-get_class_wids() {
-  for wid in ${!wid_to_class[@]}; do
-    if grep -qi "$*" <<< "${wid_to_class[$wid]}" ; then
-      echo "$wid"
-    fi
-  done
-}
+# include problematic window class names here (case insensitive)
+classes=("chromium" "discord" "slack")
 
-set_state() {
-  for wid in $(get_class_wids "$*"); do
-    # if wid is not in our dtop, hide it (attempt to force unfocus)
-    if bspc query -N -d .active -n $wid >/dev/null; then
-        echo "$*: showing!"
-        bspc node $wid -g hidden=false
-    else
-        echo "$*: hiding!"
-        bspc node $wid -g hidden=true
-    fi
-  done
-}
+bspc subscribe desktop_focus | while read -r _; do
+  #echo "Desktop changed -------"
 
-declare -A wid_to_class=();
-act() {
-  # cache
-  wid_to_class=();
-
+  # iterate over all window IDs
   for wid in $(bspc query -N -n .window); do
-	  wid_to_class[$wid]=$(xprop WM_CLASS -id "$wid")
+    wm_class="$(xprop WM_CLASS -id "$wid")"
+
+    # check if WM_CLASS matches any of the class names
+    for class in "${classes[@]}"; do
+      if grep -qi "$class" <<< "$wm_class" ; then
+        # hide or unhide the given window
+        if bspc query -N -d .active -n "$wid">/dev/null; then
+          #echo "$class: showing"
+          bspc node "$wid" -g hidden=false
+        else
+          #echo "$class: hiding"
+          bspc node "$wid" -g hidden=true
+        fi
+
+        break
+      fi
+    done
   done
 
-  # NB: add cases here:
-  set_state chromium
-  set_state discord
-  set_state slack
-}
-
-while read -r event mon_id desk_id; do
-  act
-  # Make sure the currently focused desktop becomes the previously focused
+  # make sure the currently focused desktop becomes the previously focused
   # desktop when switching
   bspc desktop --focus
-done < <(bspc subscribe desktop_focus)
+done
