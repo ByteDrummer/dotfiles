399148a4e4c301c77ee8f33091817257d95443dd
Eric Sandoval <esandovalruezga@gmail.com>
2023-06-02 23:10:04 -0700

    Merge with EOS BSPWM installer

--- setup.sh
+++ setup.sh
@@ -1,51 +1,16 @@
 #!/bin/bash
 
-set -x # show commands
+# Show commands
+set -x
 
-# update system
-sudo pacman -Syu
+# Install all required packages
+yay -S $(awk '{print}' ORS=' ' < packages.txt)
 
-# Install AUR helper
-sudo pacman -S yay
-
-# Install git
-sudo pacman -S git
-
-# Install zsh and switch shell
-sudo pacman -S zsh
+# Switch shell
 chsh -s $(which zsh)
 
-# Install zsh plugin manager
-yay -S antigen-git
-
-# Install pip
-sudo pacman -S python-pip
-
-# Install nodejs and npm
-sudo pacman -S nodejs npm
-
-# Install Java
-sudo pacman -S jre-openjdk jdk-openjdk
-
-# Install neovim
-sudo pacman -S xclip neovim # xclip is for copy paste support
-
-# Install terminal fonts
-yay -S ttf-meslo-nerd-font-powerlevel10k ttf-hack-nerd
-
-# Install Alacritty
-sudo pacman -S alacritty
-yay -S nautilus-open-any-terminal
+# Add Alacritty to Nautilus menu
 gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal alacritty
-
-# Install tmux
-sudo pacman -S tmux
-
-# Install vivid to generate LS_COLORS
-sudo pacman -S vivid
-
-# Install additional desktop software
-yay -S blueman gthumb arandr playerctl betterlockscreen caffeine-ng xidlehook light autorandr timeshift
 
 # Enable cronie service for timeshift
 sudo systemctl enable cronie.service
@@ -64,18 +29,10 @@ sudo rm /etc/systemd/system/display-manager.service
 sudo cp ~/.config/wired /etc/issue
 
 # Setup Wacom tablet
-yay -S xf86-input-wacom
 sudo cp ~/.config/wacom/99-wacom.rules /etc/udev/rules.d/
 systemctl --user enable --now wacom.service
 
-# Disable minimize, maximize, and close buttons
-gsettings set org.gnome.desktop.wm.preferences button-layout :
-
-# Setup qt theme
-yay -S kvantum qt5ct qt6ct
-
 # Setup gtk theme
-yay -S adw-gtk3
 sudo ln -s ~/.config/gtk-3.0 /root/.config/gtk-3.0
 sudo ln -s ~/.config/gtk-4.0 /root/.config/gtk-4.0
 sudo mkdir -p /root/.themes/adw-gtk3-dark
