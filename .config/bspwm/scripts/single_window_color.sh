fe5e3340fa02a5af7771b9f105b04381cec7e9a0
Eric Sandoval <esandovalruezga@gmail.com>
2023-11-17 23:09:42 -0800

    Clean up single_window_color.sh

--- .config/bspwm/scripts/single_window_color.sh
+++ .config/bspwm/scripts/single_window_color.sh
@@ -1,10 +1,8 @@
 #!/bin/env bash
 
-bspc subscribe desktop_layout desktop_focus node_add node_remove node_stack node_focus | while read -a line; do
-  #count=$(bspc query -N -d focused -n .window.\!hidden | wc -l)
-  count=$(bspc query -N -d focused -n .window | wc -l) # works better with electron fix
-  echo $count
-  if [ $count == 1 ]; then
+bspc subscribe desktop_layout desktop_focus node_add node_remove node_stack node_focus | while read -r _; do
+  count=$(bspc query -N -d focused -n .window | wc -l)
+  if [ "$count" == 1 ]; then
     bspc config focused_border_color "#282c34"
   else
     bspc config focused_border_color "#abb2bf"
