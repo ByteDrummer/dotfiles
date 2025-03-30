7594dd537abbda5289c240faca2423ce0d10a31d
Eric Sandoval <esandovalruezga@gmail.com>
2025-03-30 13:19:26 -0700

    Fix barbar colors

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -34,6 +34,10 @@ return {
       require('onedark').setup(opts)
       require('onedark').load()
       vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#393f4a", fg = "NONE" })
+      vim.api.nvim_set_hl(0, "BufferCurrent", { bg = "#282c34", fg = "#aab2bf", bold = true})
+      vim.api.nvim_set_hl(0, "BufferCurrentSign", { bg = "#282c34", fg = "#c678dd"})
+      vim.api.nvim_set_hl(0, "BufferCurrentMod", { bg = "#282c34", fg = "#d19a66", bold = true, italic = true})
+      vim.api.nvim_set_hl(0, "BufferInactive", { bg = "#31353f", fg = "#848b98"})
     end
   },
 
