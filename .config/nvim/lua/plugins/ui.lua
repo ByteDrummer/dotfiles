f83825cc99ee6a3a0d830a6c8f630fc035eebe4c
Eric Sandoval <esandovalruezga@gmail.com>
2025-09-22 14:15:31 -0700

    Add render-markdown plugin

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,5 +1,13 @@
 return {
   {
+    'MeanderingProgrammer/render-markdown.nvim',
+     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+
+  {
     'nvim-telescope/telescope.nvim',
     dependencies = {
       { 'nvim-lua/plenary.nvim' },
