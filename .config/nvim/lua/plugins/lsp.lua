4cab07482d9ae7721afaecea62d9b9a944b7a598
Eric Sandoval <esandovalruezga@gmail.com>
2025-10-10 13:49:48 -0700

    Improve completion keybinds

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -25,13 +25,8 @@ return {
       { 'onsails/lspkind.nvim' },
     },
     config = function()
-      -- Here is where you configure the autocompletion settings.
-      local lsp_zero = require('lsp-zero')
-      lsp_zero.extend_cmp()
-
-      -- And you can configure cmp even more, if you want to.
       local cmp = require('cmp')
-      local cmp_action = lsp_zero.cmp_action()
+      local luasnip = require("luasnip")
 
       require('luasnip.loaders.from_vscode').lazy_load()
       require('luasnip').filetype_extend("javascript", { "javascriptreact" })
@@ -44,8 +39,40 @@ return {
           { name = 'luasnip', keyword_length = 2 },
         },
         mapping = {
-          ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-          ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+          ["<C-y>"] = cmp.mapping(
+            cmp.mapping.confirm({
+              select = true,
+              behavior = cmp.ConfirmBehavior.Insert,
+            }),
+            { "i", "c" }
+          ),
+          ["<C-n>"] = cmp.mapping.select_next_item({
+            behavior = cmp.ConfirmBehavior.Insert,
+          }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({
+            behavior = cmp.ConfirmBehavior.Insert,
+          }),
+          ["<C-f>"] = cmp.mapping.scroll_docs(5),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-5),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.locally_jumpable(1) then
+              luasnip.jump(1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         },
         window = {
           completion = {
