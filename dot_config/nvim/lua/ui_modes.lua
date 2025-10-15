43184237a5ff4f7141bff05334372996ba0e3bc1
Eric Sandoval Ruezga <eric.ruezga@wisk.aero>
2025-10-15 10:14:16 -0700

    Disable bufferline offset

--- dot_config/nvim/lua/ui_modes.lua
+++ dot_config/nvim/lua/ui_modes.lua
@@ -6,18 +6,18 @@ return {
     self.debug_open = false
 
     vim.cmd("NvimTreeToggle")
-    if require 'nvim-tree.view'.is_visible() then
-      require 'bufferline.api'.set_offset(34, '')
-    else
-      require 'bufferline.api'.set_offset(0)
-    end
+    --if require 'nvim-tree.view'.is_visible() then
+      --require 'bufferline.api'.set_offset(34, '')
+    --else
+      --require 'bufferline.api'.set_offset(0)
+    --end
   end,
 
   outline_toggle = function(self)
     if self.debug_open then
       require 'dapui'.close()
       self.debug_open = false
-      require 'bufferline.api'.set_offset(0)
+      --require 'bufferline.api'.set_offset(0)
     end
 
     vim.cmd('SymbolsOutline')
@@ -31,10 +31,10 @@ return {
     require 'dapui'.toggle { reset = true }
     self.debug_open = not self.debug_open
 
-    if self.debug_open then
-      require 'bufferline.api'.set_offset(40, '')
-    else
-      require 'bufferline.api'.set_offset(0)
-    end
+    --if self.debug_open then
+      --require 'bufferline.api'.set_offset(40, '')
+    --else
+      --require 'bufferline.api'.set_offset(0)
+    --end
   end
 }
