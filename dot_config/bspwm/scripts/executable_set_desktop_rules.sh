32bc098bfa417b4cc04b28721c58bb2d790d7574
Eric Sandoval <esandovalruezga@gmail.com>
2026-02-18 21:37:05 -0800

    Add custom desktop files for Google Calendar and Google Messages

--- dot_config/bspwm/scripts/executable_set_desktop_rules.sh
+++ dot_config/bspwm/scripts/executable_set_desktop_rules.sh
@@ -3,8 +3,8 @@
 primary_monitor=$(xrandr | grep primary | sed 's/ .*//')
 
 bspc rule -a Geary desktop="$primary_monitor:^1"
-bspc rule -a "*:crx_kjbdgfilnfhdoflbpgamdcdgpehopbep" desktop="$primary_monitor:^1"
-bspc rule -a "*:crx_hpfldicfbfomlpcikngkocigghgafkph" desktop="$primary_monitor:^2"
+bspc rule -a "*:calendar.google.com" desktop="$primary_monitor:^1"
+bspc rule -a "*:messages.google.com" desktop="$primary_monitor:^2"
 bspc rule -a discord desktop="$primary_monitor:^2"
 bspc rule -a Slack desktop="$primary_monitor:^2"
 bspc rule -a Lutris desktop="$primary_monitor:^5"
