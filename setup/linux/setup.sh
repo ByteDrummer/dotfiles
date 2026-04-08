4eb64d54b8660c68a3eb5e5a2a8ef34e02000659
Eric Sandoval <esandovalruezga@gmail.com>
2026-04-07 21:08:47 -0700

    Add mouse udev rules

--- setup/linux/setup.sh
+++ setup/linux/setup.sh
@@ -50,6 +50,9 @@ sudo cp powersave.conf /etc/NetworkManager/conf.d/powersave.conf
 # Disable mouse acceleration
 sudo cp 40-disable-accel.conf /etc/X11/xorg.conf.d/
 
+# Add mouse udev rules
+sudo cp 71-pulsar.rules /etc/udev/rules.d/
+
 # Setup root gtk theme
 sudo mkdir -p /root/.config
 sudo mkdir -p /root/.themes
