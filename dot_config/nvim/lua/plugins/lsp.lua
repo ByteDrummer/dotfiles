07f1577542acb1eb59fe448d9c3e5a5f45907fee
Eric Sandoval <esandovalruezga@gmail.com>
2026-05-19 22:59:11 -0700

    Add nvim-lsp-file-operations nvim-tree extension

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,5 +1,16 @@
 return {
   {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-tree.lua",
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
+  },
+
+  {
     "folke/lazydev.nvim",
     ft = "lua", -- only load on lua files
     opts = {},
